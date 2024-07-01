b31847d8c73d5fb1ef58bb063d6eabd6368ef235
zijie.hong1@gmail.com <zijie.hong1@gmail.com>
2024-07-01 17:46:30 +0000

    Edited main and main_copy

--- client_code/main/__init__.py
+++ client_code/main/__init__.py
@@ -2,12 +2,14 @@ from ._anvil_designer import mainTemplate
 from anvil import *
 import plotly.graph_objects as go
 from ..homepage import homepage
+from ..main_copy import main_copy
 
 class main(mainTemplate):
     def __init__(self, **properties):
         # 初始化组件
         self.init_components(**properties)
-        self.content_panel.add_component(homepage(), full_width_row=True)
+        #self.content_panel.add_component(homepage(), full_width_row=True)
+        self.content_panel.add_component(main_copy(), full_width_row=True)
        
 
 
--- client_code/main_copy/__init__.py
+++ client_code/main_copy/__init__.py
@@ -8,4 +8,4 @@ class main_copy(main_copyTemplate):
   def __init__(self, **properties):
     # 初始化组件
     self.init_components(**properties)
-    self.content_panel.add_component(homepage(), full_width_row=True)
+ 
