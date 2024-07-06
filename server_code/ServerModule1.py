9ecf8040ec0f48a9cfb4482968da71fef9b16216
zijie.hong1@gmail.com <zijie.hong1@gmail.com>
2024-07-06 13:41:58 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,15 +3,6 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+def get_section_headings():
+    rows = app_tables.section_headings.search()
+    return [r.to_dict() for r in rows]
