d0cfaf6890118fdf86b09a67931dc3a097b3692b
zijie.hong1@gmail.com <zijie.hong1@gmail.com>
2024-07-06 15:54:21 +0000

    Edited word and ServerModule1

--- client_code/word/__init__.py
+++ client_code/word/__init__.py
@@ -1,6 +1,6 @@
 from ._anvil_designer import wordTemplate
 from anvil import *
-
+import anvil.server
 
 class word(wordTemplate):
   def __init__(self, data=None, **properties):
@@ -10,6 +10,13 @@ class word(wordTemplate):
     print(self.data_list)
     self.current_index = 0
     self.update_display()
+
+    if len(self.data_list) <= 1:
+            self.button_1.visible = False
+            self.button_2.visible = False
+    else:
+            self.button_1.visible = False
+            self.button_2.visible = True
     
   def update_display(self):
       if self.data_list and 0 <= self.current_index < len(self.data_list):
@@ -19,12 +26,21 @@ class word(wordTemplate):
           self.label_word_class.text = data.get('word_class', '')
           self.label_metaphor_meaning.text = data.get('metaphor_meaning', '')
           self.label_english_example_sentence.text = data.get('english_example_sentence', '')
+
+          section_heading_id = data.get('section_heading_id')
+          if section_heading_id:
+                main_heading_data = anvil.server.call('get_main_heading_data', section_heading_id)
+                if main_heading_data:
+                    self.label_main_heading.text = main_heading_data['main_heading']
       else:
           self.label_item.text = 'No data found'
           self.label_literal_meaning.text = ''
           self.label_word_class.text = ''
           self.label_metaphor_meaning.text = ''
           self.label_english_example_sentence.text = ''
+
+      self.button_1.visible = self.current_index > 0
+      self.button_2.visible = self.current_index < len(self.data_list) - 1
           
   def button_1_click(self, **event_args):
       if self.current_index > 0:
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -6,7 +6,7 @@ import anvil.server
 @anvil.server.callable
 def search_lexical_items(headword):
     headword = headword.strip() 
-    matching_rows = app_tables.lexical_items.search(english_headword=q.ilike(f"%{headword}%"))
+    matching_rows = app_tables.lexical_items.search(english_headword=headword)
     results = []
     for row in matching_rows:
         results.append({
@@ -22,3 +22,27 @@ def search_lexical_items(headword):
         return results
     else:
         return None
+
+@anvil.server.callable
+def get_main_heading_data(section_heading_id):
+    # 查询 section_headings 表，获取 main_heading_id
+    section_heading_row = app_tables.section_headings.get(section_heading_id=section_heading_id)
+    if not section_heading_row:
+        return None
+    
+    main_heading_id = section_heading_row['main_heading_id']
+    
+    main_heading_row = app_tables.main_headings.get(main_heading_id=main_heading_id)
+    if not main_heading_row:
+        return None
+    
+    main_heading_data = {
+        "main_heading_id": main_heading_row['main_heading_id'],
+        "main_heading": main_heading_row['main_heading'],
+        "target_id": main_heading_row['target_id'],
+        "source_id": main_heading_row['source_id'],
+        "category_source_id": main_heading_row['category_source_id'],
+        "category_target_id": main_heading_row['category_target_id']
+    }
+    
+    return main_heading_data
