3d04ec441ac019d37dcb4cf0352afd6fdd3a89fd
zijie.hong1@gmail.com <zijie.hong1@gmail.com>
2024-08-05 20:50:50 +0000

    Edited about_us

--- client_code/about_us/__init__.py
+++ client_code/about_us/__init__.py
@@ -10,5 +10,6 @@ class about_us(about_usTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+ 
 
     # Any code you write here will run before the form opens.
