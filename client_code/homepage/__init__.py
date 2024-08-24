70269c0d25d91ebc0b5d7d8010f0ebfb1fddb7ba
zijie.hong1@gmail.com <zijie.hong1@gmail.com>
2024-08-24 13:34:33 +0000

    Edited homepage

--- client_code/homepage/__init__.py
+++ client_code/homepage/__init__.py
@@ -7,7 +7,6 @@ class homepage(homepageTemplate):
   def __init__(self, **properties):
       self.init_components(**properties)
       
-
   def search_lexical_item_button_click(self, **event_args):
     user_input = self.outlined_1.text.strip().lower()
     search_lexical_item(user_input)
@@ -15,29 +14,14 @@ class homepage(homepageTemplate):
       
   def search_theme_button_click(self, **event_args):
       result = None
-      if self.radio_theme.selected:
-          input1 = self.input_box_1.text.strip().upper()
-          input2 = self.input_box_2.text.strip().upper()
-          if input1 and input2:
-            result = anvil.server.call('search_by_theme', input1, input2)
-          else:
-            alert("Please input both fields")
-            return
-      elif self.radio_target.selected:
-          input = self.input_box_1.text.strip().upper()
-          if input:
-            result = anvil.server.call('search_themes_by_target', input)
-          else:
-            alert("Please input a target")
-            return
-      elif self.radio_source.selected:
-          input = self.input_box_2.text.strip().upper()
-          if input:
-            result = anvil.server.call('search_themes_by_source', input)
-          else:
-            alert("Please input a source.")
-            return
-        
+      input1 = self.input_box_1.text.strip().upper()
+      input2 = self.input_box_2.text.strip().upper()
+      if input1 and input2:
+        result = anvil.server.call('search_by_theme', input1, input2)
+      else:
+        alert("Please input both fields")
+        return
+              
       if result:
           open_form('LexicalItem', theme_panel_role='elevated-card', item_panel_visibility=False, data=result)
       else:
@@ -60,6 +44,32 @@ class homepage(homepageTemplate):
     self.column_panel_2.add_component(self.input_box_1)
     self.column_panel_2.add_component(self.label_2)
     self.column_panel_2.add_component(self.input_box_2)
+
+  def search_target_button_click(self, **event_args):
+    result = None
+    input = self.target_input_box.text.strip().upper()
+    if input:
+        result = anvil.server.call('search_themes_by_target', input)
+    else:
+        alert("Please input a target")
+        return
+    if result:
+        open_form('LexicalItem', theme_panel_role='elevated-card', item_panel_visibility=False, data=result)
+    else:
+        alert("No results found.")
+
+  def search_source_button_click(self, **event_args):
+    result = None
+    input = self.source_input_box.text.strip().upper()
+    if input:
+      result = anvil.server.call('search_themes_by_source', input)
+    else:
+      alert("Please input a source.")
+      return
+    if result:
+        open_form('LexicalItem', theme_panel_role='elevated-card', item_panel_visibility=False, data=result)
+    else:
+        alert("No results found.")
 
 
 
